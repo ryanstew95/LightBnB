96e5106153a709daccab4dbd93d5cb597d6dd709
Ryan Stewart <ryanstew17@gmail.com>
2023-07-18 17:53:04 -0300

    adding function details back in

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -9,6 +9,12 @@ const pool = new Pool({
 
 /// Users
 
+/**
+ * Get a single user from the database given their email.
+ * @param {String} email The email of the user.
+ * @return {Promise<{}>} A promise to the user.
+ */
+
 const getUserWithEmail = function (email) {
   return pool
     .query("SELECT * FROM users WHERE email = $1", [email])
@@ -21,6 +27,12 @@ const getUserWithEmail = function (email) {
     });
 };
 
+/**
+ * Get a single user from the database given their id.
+ * @param {string} id The id of the user.
+ * @return {Promise<{}>} A promise to the user.
+ */
+
 const getUserWithId = function (id) {
   return pool
     .query("SELECT * FROM users WHERE id = $1", [id])
@@ -29,6 +41,12 @@ const getUserWithId = function (id) {
     })
     .catch((err) => console.log(err));
 };
+
+/**
+ * Add a new user to the database.
+ * @param {{name: string, password: string, email: string}} user
+ * @return {Promise<{}>} A promise to the user.
+ */
 
 const addUser = function(user) {
   return pool
@@ -43,6 +61,12 @@ const addUser = function(user) {
 };
 
 /// Reservations
+
+/**
+ * Get all reservations for a single user.
+ * @param {string} guest_id The id of the user.
+ * @return {Promise<[{}]>} A promise to the reservations.
+ */
 
 const getAllReservations = function(guestId, limit = 10) {
   const queryString = `
@@ -64,6 +88,13 @@ const getAllReservations = function(guestId, limit = 10) {
 };
 
 /// Properties
+
+/**
+ * Get all properties.
+ * @param {{}} options An object containing query options.
+ * @param {*} limit The number of results to return.
+ * @return {Promise<[{}]>}  A promise to the properties.
+ */
 
 const getAllProperties = function(options, limit = 10) {
 
@@ -128,6 +159,12 @@ const getAllProperties = function(options, limit = 10) {
     })
     .catch((err) => {});
 };
+
+/**
+ * Add a property to the database
+ * @param {{}} property An object containing all of the property details.
+ * @return {Promise<{}>} A promise to the property.
+ */
 
 const addProperty = function (property) {
   const queryParams = [
